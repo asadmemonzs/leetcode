b010b3724e6fb8f0ca0afe87a89ff651485f9862
asadmemonzs <asadmemonwork@gmail.com>
2023-09-19 21:55:22 +0500

    added the solution for search insert position

---
+++ Solutions/src/com/searchinsertposition/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.searchinsertposition;
+
+/**
+ *
+ * @author ShahJE
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1,3,5,6,7,9,11,12,15,18};
+        System.out.println(solution.searchInsert(nums,8));
+    }
+}
---
+++ Solutions/src/com/searchinsertposition/Solution.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.searchinsertposition;
+
+/**
+ *
+ * @author asadmemonzs
+ */
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
